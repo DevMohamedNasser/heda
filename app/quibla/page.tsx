b92891526c5b0bf3332487d57747ebe2f32b0f21
DevMohamedNasser <m.nasser.dev@gmail.com>
2025-12-23 19:17:09 +0200

    test

--- app/quibla/page.tsx
+++ app/quibla/page.tsx
@@ -133,7 +133,7 @@ export default function QiblaPage() {
       </div>
       <p className="mt-4 text-gray-700 dark:text-gray-300 text-sm max-w-sm text-center">
         ⚠️ اتجاه القبلة يعتمد على البوصلة وقد يتأثر بالمجال المغناطيسي للأرض.
-        يُفضل معايرة الهاتف بتحريكه على شكل رقم 8.
+        يُفضل معايرة الهاتف بتحريكه على شكل رقم ٥.
       </p>
     </div>
   );
